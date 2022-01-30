257199949fdf4d07408162b0f8bfc4339e14c2bc
marshalwelgama <marshalwelgama@gmail.com>
2022-01-30 20:30:16 +1100

    new: added time waiting in minutes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,30 +1,22 @@
 import React, { Component } from "react";
+import moment from "moment";
 import "bootstrap/dist/css/bootstrap.css";
 
 class Counter extends React.Component {
   constructor() {
     super();
     this.state = {
-      curTime: null,
+      timeNow: moment().format()
     };
   }
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        curTime: new Date().toLocaleString(),
-      });
-    }, 1000);
-  }
+
   //   handleClick = () => {
   //     this.setState({ value: this.state.value + 1 });
   //   };
   render() {
     return (
       <React.Fragment>
-        <span className="badge bg-primary">
-          {this.props.timeExac - this.state.curTime}
-        </span>
-        <span className="badge bg-primary">{this.props.timeExac}</span>
+        <span className="badge bg-primary">{moment(this.props.timeExac).diff(this.state.timeNow, 'minutes')}</span>
         <span>{this.props.counter.value}</span>
         <button onClick={() => this.props.onDelete(this.props.counter.id)}>
           Delete
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
+import moment from "moment";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: "james upgd 30 min" },
-      { id: 2, value: 5 },
-      { id: 3, value: 5 },
-    ],
-  };
+  constructor() {
+    super()
+    this.state = {
+
+        counters: [
+          { id: 1, value: "james upgd 30 min" },
+          { id: 2, value: 5 },
+          { id: 3, value: 5 },
+        ],
+    }
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        curTime: moment().format(),
+      });
+    }, 1000);
+  }
+ 
   handleDelete = (countId) => {
     const counters = this.state.counters.filter((c) => c.id !== countId);
     this.setState({ counters });
@@ -24,7 +37,7 @@ class Counters extends Component {
                 key={counter.id}
                 counter={counter}
                 onDelete={this.handleDelete}
-                timeExac={new Date().toLocaleString()}
+                timeExac={this.state.curTime}
               />
             </li>
           ))}
